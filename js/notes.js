6e8a39eebd9fe007e988c79af8fe4a6d65cadfc0
TaylorMade <warriorotk18@gmail.com>
2022-08-16 14:01:39 -0600

    Got a healthy chunck of great string methods

--- js/notes.js
+++ js/notes.js
@@ -21,5 +21,22 @@ const mathVariable = "Mathematics";
 console.log(mathVariable.length);
 
 // String Methods
+// --------------------
+
 // charAt()-This method allows us to see which postion the character is in the string - index positioning
 console.log(mathVariable.charAt(6));
+
+// indexOf()-This method returns the position of the first occurrence of a value in a string
+console.log(mathVariable.indexOf('th'));
+// lastIndexOf()-is identical only returning the position of the 'last' occurance of the value
+console.log(mathVariable.lastIndexOf('t'));
+
+// slice()-this method returns the starting through the ending characters, slices them right out!
+console.log(mathVariable.slice(2, 6));
+
+// includes()-returns a boolean on whether string 'includes' value or not
+console.log(mathVariable.includes('ath'));
+
+// split()-method splits a string into an array of substrings. Whatever character for the value is NOT included in the resulting substrings
+console.log("This page is coming out great!".split(" "));
+console.log('Cecil, Kain, Rydia, Edgar, Tellor'.split(", "));
