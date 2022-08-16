e5ed50390b22a34de2deed49e43cdf96eb6d2cd3
TaylorMade <warriorotk18@gmail.com>
2022-08-16 13:11:08 -0600

    notes on 4 different data types

---
+++ app.js
@@ -0,0 +1,14 @@
+// 4 Different Data Types
+
+// #1-String
+console.log(typeof "TaylorMade");
+
+// #2-Numbers
+console.log(typeof 18);
+
+// #3-Boolean
+console.log(18 + 2 === 21);
+
+// #4-undefined
+var undef;
+console.log(undef);
