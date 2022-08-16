353206b198a1de65d3e2ecf89cf26fa82e884047
TaylorMade <warriorotk18@gmail.com>
2022-08-16 13:30:41 -0600

    notes on string properties and methods - length, charAt()

--- js/notes.js
+++ js/notes.js
@@ -1,3 +1,4 @@
+// 8/16/22
 // 4 Different Data Types
 
 // #1-String
@@ -12,3 +13,13 @@ console.log(18 + 2 === 21);
 // #4-undefined
 var undef;
 console.log(undef);
+
+// ----Strings----
+const mathVariable = "Mathematics";
+
+// The length property
+console.log(mathVariable.length);
+
+// String Methods
+// charAt()-This method allows us to see which postion the character is in the string - index positioning
+console.log(mathVariable.charAt(6));
