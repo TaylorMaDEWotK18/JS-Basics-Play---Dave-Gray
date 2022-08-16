ea8760d4813894fefcb6b5a58ad92f4e493bf70d
TaylorMade <warriorotk18@gmail.com>
2022-08-16 17:24:25 -0600

    intro problem was difficult to understand at first with loop

--- js/eloquentjs.js
+++ js/eloquentjs.js
@@ -1,1 +1,10 @@
 // This is a sandbox for the exercises in Eloquent JavaScript. LETS GET IT!!
+
+// Intro:
+let total = 0, count = 1;
+while(count <= 10) {
+	total += count;
+	count += 1;
+}
+
+console.log(total);
