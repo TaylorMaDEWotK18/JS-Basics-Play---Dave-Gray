962f41fda232954fce334794f0dcb0414d4a1eda
TaylorMade <warriorotk18@gmail.com>
2022-08-18 16:42:24 -0600

    variables set and ready to go

--- js/app.js
+++ js/app.js
@@ -1,3 +1,7 @@
-// One of the early challenges for 'variables and strings' from Treehouse
+// One of the early challenges for 'variables and strings' from Treehouse. We have to set up a few rounds of questions to the user and spit back out answers in funny sentence.
 
 // 1.Declare vaiables and capture input.
+let name = prompt('What is your name my friend?');
+let age = prompt("How many years have you been on this earth?");
+let animal = prompt('What is your favorite animal?');
+let season = prompt("What is your favorite season!?");
