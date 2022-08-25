23dd89c8697b133d63f704e8aa764e3d11bdd925
TaylorMade <warriorotk18@gmail.com>
2022-08-25 14:27:09 -0600

    random method and tried to apply floor and ceil not doing it correctly

--- js/chp5.js
+++ js/chp5.js
@@ -1,5 +1,5 @@
 //Math Objects
-// trunc, round, ceil and floor methods ----------------------
+// --------------TRUNC, ROUND, CEIL AND FLOOR------------------------
 
 //trunc---Returns the integer portion of any number and truncates the decimals
 console.log(Math.trunc(Math.PI));
@@ -13,9 +13,23 @@ console.log(Math.ceil(4.346));
 //floor---Will always round number DOWN
 console.log(Math.floor(35.7850));
 
-//---------POW!!!! Method--------------
+//-------------------POW!!!! Method------------------------
+
 //The POW method takes 2 arguments, the base number and the exponent 
 //"TO THE POWER OF!"
 console.log(Math.pow(4, 4));
 console.log(Math.pow(3, 3));
 console.log(Math.pow(5, 10));
+
+//--------------------MIN and MAX---------------------------
+
+//Will return the smallest number / largest number
+console.log(Math.min(3, 43, 12));
+console.log(Math.max(23, 324, 2343));
+
+//----------------------RANDOM------------------------------
+
+//It provides a pseudo random number from 0 - 1
+console.log(Math.random() * 6);
+console.log(Math.random(Math.floor) * 18);
+console.log(Math.random(Math.ceil) * 1000);
