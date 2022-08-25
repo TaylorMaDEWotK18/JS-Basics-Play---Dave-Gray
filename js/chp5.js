7668394da2127326c00370ceff86bd06a7a53d85
TaylorMade <warriorotk18@gmail.com>
2022-08-25 14:09:52 -0600

    trunc, round, ceil and floor methods

--- js/chp5.js
+++ js/chp5.js
@@ -1,1 +1,14 @@
 //Math Objects
+// trunc, round, ceil and floor methods ----------------------
+
+//trunc---Returns the integer portion of any number and truncates the decimals
+console.log(Math.trunc(Math.PI));
+
+//round---Rounds the floating decimals to integer
+console.log(Math.round(125.6354345));
+
+//ceil-(ceiling) Will always round the number UP
+console.log(Math.ceil(4.346));
+
+//floor---Will always round number DOWN
+console.log(Math.floor(35.7850));
